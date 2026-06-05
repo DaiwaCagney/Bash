02acbd1e848295ee54f2f795ee69e5c42de06540
DaiwaCagney <120650386+DaiwaCagney@users.noreply.github.com>
2026-06-05 17:45:27 +0800

    Create NmapScan.sh

---
+++ NmapScan.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+filename=$(date +"%d%m%Y%H%M").txt
+
+while read -r line
+do
+        echo "checking $line"
+        nmap -Pn -sV --scan-delay 5s "$line" | grep tcp > nmapoutput.txt
+        echo " "
+        while read -r line2
+        do
+                echo "$line $line2" >> "$filename"
+        done < nmapoutput.txt
+        echo "Done $line"
+done < IP.txt
