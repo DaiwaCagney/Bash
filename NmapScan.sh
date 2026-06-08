630d921137e3438f30470686502cfbebd8d29a43
DaiwaCagney <120650386+DaiwaCagney@users.noreply.github.com>
2026-06-08 11:40:21 +0800

    Update NmapScan.sh

--- NmapScan.sh
+++ NmapScan.sh
@@ -6,10 +6,13 @@ while read -r line
 do
         echo "checking $line"
         nmap -Pn -sV --scan-delay 5s "$line" | grep tcp > nmapoutput.txt
-        echo " "
         while read -r line2
         do
                 echo "$line $line2" >> "$filename"
         done < nmapoutput.txt
         echo "Done $line"
 done < IP.txt
+
+rm -f nmapoutput.txt
+
+echo "Finished" >> "$filename"
